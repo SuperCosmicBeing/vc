7b61ed949dd34cd60b17f908e319214b9eb1dc18
Shéikh Adnan <manofuranium@gmail.com>
2021-03-15 14:56:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+api_id = 3658600
+api_hash = "28b5c1a0fe98e995a62b951ad14f22fb"
+sudo_chat_id = -1001490063243
+owner_id = 1549401069
+
+
+# Arq Api, don't make changes here
+ARQ = "https://thearq.tech/"
